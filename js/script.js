f2a93494ca17a5c6b7df26d6292da735c81e611a
developer <developer@email.com>
2020-09-25 21:12:12 +0000

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+// التنثل بأسلوب سلس إلى الفقرة الهدف
 
+$(document).ready(function(){
+    $("a.scroll").on('click', function(event){
+
+        var hash = this.hash;
+
+        $('html,body').animate({scrollTop: $(hash).offset().top},800,function(){})
+    });
+
+    //للعداد في قسم إحصائيات الشركة
+    $('.timer').countTo();
+
+    //ضبط عناصر التحقق لدخل المستخدم
+    $('#commentForm').validate();
+});
